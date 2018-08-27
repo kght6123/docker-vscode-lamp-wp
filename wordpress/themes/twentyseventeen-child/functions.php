1b2e6dc30eb5f33f4a9846f5b9a75de0dbb9898d
hirotaka.koga <i.feeling.song+git@gmail.com>
2018-08-27 23:42:14 +0900

    JQueryの有効化、スタイル微調整

--- wordpress/themes/twentyseventeen-child/functions.php
+++ wordpress/themes/twentyseventeen-child/functions.php
@@ -6,45 +6,10 @@ function kght_theme_enqueue_styles() {
 
 	// 親CSSを継承して、子CSSの読み込み
 	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
-
-	date_default_timezone_set('Asia/Tokyo');
-
-	$kght_style_mode = get_query_var('kght_style_mode', 'none');
-	$time_hour = idate('H');
-
-	// http://localhost:28080/?kght_style_mode=light
-	// http://localhost:28080/?kght_style_mode=dark
-
-	if (strcmp($kght_style_mode, 'light') == 0) {
-		kght_light_enqueue_styles();
-	} elseif (strcmp($kght_style_mode, 'dark') == 0) {
-		kght_dark_enqueue_styles();
-	} elseif (5 <= $time_hour && $time_hour <= 17) { // 4時～17時の時間帯のとき
-		kght_light_enqueue_styles();
-	} elseif (17 <= $time_hour && $time_hour <= 24) { // 17時～24時の時間帯のとき
-		kght_dark_enqueue_styles();
-	} else {
-		kght_dark_enqueue_styles();
-	}
+	
 }
-function kght_dark_enqueue_styles(){
-	// ダークテーマの読み込み
-	wp_enqueue_style('dark-style', get_stylesheet_directory_uri() . '/dark.css', array('child-style'));
-}
-function kght_light_enqueue_styles(){
-	// ライトテーマの読み込み
-	wp_enqueue_style('light-style', get_stylesheet_directory_uri() . '/light.css', array('child-style'));
-}
-function kght_add_query_vars_filter( $vars ){
-	$vars[] = "kght_style_mode";
-	return $vars;
-}
-
 // テーマスタイル読み込みアクションをスクリプトキューに追加
 add_action('wp_enqueue_scripts', 'kght_theme_enqueue_styles');
-
-// カスタムクエリ変数を追加
-add_filter('query_vars', 'kght_add_query_vars_filter');
 
 // wordpressにインストールされているjqueryを無効にする関数
 // add_filter('init',function(){
